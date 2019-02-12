997e70647ab83ddc86214e7be83646a870c08b81
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-02-12 23:00:11 +0000

    Video 14 Completed: Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,16 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    // create a dog
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
   }
 };
 
