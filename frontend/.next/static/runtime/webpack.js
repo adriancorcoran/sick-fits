e139e40fb561c4de3a8ec983d9fbdf984e57fc0b
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-06-27 00:13:18 +0100

    Video 17 Complete: Creating Items with Mutations

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "85756fca0238e6c1df5e";
+/******/ 	var hotCurrentHash = "8566201309bc84c4e4c7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
